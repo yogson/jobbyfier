038476295776fe8fa58ee7f88f4a10a75f1e671f
Ig0oo <yogson@gmail.com>
2020-04-07 19:48:51 +0300

    feat: Fulltext analysis

--- analysis.py
+++ analysis.py
@@ -27,12 +27,15 @@ def get_collection_full_text(collection_name):
     return full_text
 
 
-def get_top_words(text):
+def get_top_words(text, eng_only=True):
 
     def trans(chars):
         return str.maketrans(dict(zip(chars, list(' ' * len(chars)))))
 
-    trans_tab = trans(list(string.punctuation) + list('\r\n«»\–') + list(RUSSIAN_ALPHABET) + list(string.digits))
+    if eng_only:
+        trans_tab = trans(list(string.punctuation) + list('\r\n«»\–') + list(RUSSIAN_ALPHABET) + list(string.digits))
+    else:
+        trans_tab = trans(list(string.punctuation) + list('\r\n«»\–'))
 
     df = pd.DataFrame({
         'comm': re.split(r'[\n\r\.\?!]', text)
@@ -45,8 +48,11 @@ def get_top_words(text):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) > 1:
         collection = 'vacancies_'+sys.argv[1]
-        top = get_top_words(get_collection_full_text('collection'))
+        if len(sys.argv) == 3 and sys.argv[2] == 'eng':
+            top = get_top_words(get_collection_full_text('collection'))
+        else:
+            top = get_top_words(get_collection_full_text('collection'), eng_only=False)
         for word, count in top.most_common(50):
             print(f'{word}: \t{count}')
