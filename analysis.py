9081deee96b76e5fd9ac75bec6e8545f2212767c
Ig0oo <yogson@gmail.com>
2020-04-07 19:27:14 +0300

    feat: Claculate_all for single base

---
+++ analysis.py
@@ -0,0 +1,54 @@
+import os
+import re
+from collections import defaultdict, Counter
+
+import pandas as pd
+import nltk
+from nltk import word_tokenize, sent_tokenize, FreqDist
+import string
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.jobs
+
+
+def clean_html(raw_html):
+    cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    cleantext = re.sub(cleanr, ' ', raw_html)
+    return cleantext
+
+
+def get_collection_full_text(collection_name):
+    collection = db.get_collection('vacancies_'+collection_name)
+    qs = collection.find()
+    i = 20
+    full_text = ''
+    while i:
+        full_text += '\n'+clean_html(qs[i].get('description'))
+        i -= 1
+    return full_text
+
+
+def get_top_words(text):
+
+    def trans(chars):
+        return str.maketrans(dict(zip(chars, list(' ' * len(chars)))))
+
+    trans_tab = trans(list(string.punctuation) + list('\r\n«»\–'))
+
+    df = pd.DataFrame({
+        'comm': re.split(r'[\n\r\.\?!]', text)
+    })
+    df['comm'] = df['comm'].str.translate(trans_tab).str.lower()
+
+    words = [word for word in word_tokenize(df['comm'].str.cat(sep=' '))]
+
+    return FreqDist(words)
+
+
+
+
+
+top = get_top_words(get_collection_full_text('python'))
+
+print(top.most_common(20))
